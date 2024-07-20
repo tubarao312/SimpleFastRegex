8cc5fc046bd3ae9485d9df54777374722f3581af
Pedro Ribeiro <47680931+tubarao312@users.noreply.github.com>
2024-07-20 01:39:26 +0100

    Update example.py

--- example.py
+++ example.py
@@ -1,6 +1,29 @@
 import simple_fast_regex as sfr
+import regex as re
+import time
 
+TEXT = "This is a test"
+
+# Rust Implementation
+start = time.time()
 engine = sfr.RegexEngine(["test", "this pattern doesn't exist"])
-text = "This is a test"
+rust_compile_time = time.time() - start
+print(f"Rust took {rust_compile_time} seconds to compile the Regex.")
 
-print(engine.get_pattern_matches(text))  # [0], meaning the first pattern was found
+start = time.time()
+engine.get_pattern_matches(TEXT) # [0] because only the first pattern matches
+rust_match_time = time.time() - start
+print(f"Rust took {time.time() - start} seconds to run compiled Regex.")
+
+print(f"----------------------------------")
+
+# Python implementation
+start = time.time()
+compiled_patterns = [re.compile("test"), re.compile("this pattern doesn't exist")]
+python_compile_time = time.time() - start
+print(f"Python took {python_compile_time} seconds to compile the Regex.")
+
+start = time.time()
+matches = [pattern.search(TEXT) for pattern in compiled_patterns]
+python_match_time = time.time() - start
+print(f"Python took {time.time() - start} seconds to run compiled Regex.")
